825d7704503bbbf5f6f69e4f3b324bafd4fd931c
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-04-29 16:54:13 +0300

    feat: add all keyboard buttons

--- src/index.js
+++ src/index.js
@@ -3,10 +3,38 @@ import './assets/fonts/fonts.css';
 import Btn from './scripts/btn_class.js';
 import btnValues from './assets/btn_values.json';
 
-function createHeader(text) {
+const lang = 'en';
+
+// function setLocalStorage() {
+//   localStorage.setItem('language', lang);
+// }
+// window.addEventListener('beforeunload', setLocalStorage);
+
+// function getLocalStorage() {
+//   if (localStorage.getItem('language') === 'undefined') {
+//     lang = 'en';
+//   } else {
+//     lang = localStorage.getItem('language');
+//     console.log(lang);
+//   }
+// }
+
+// window.addEventListener('load', getLocalStorage);
+
+function createHeader() {
   const header = document.createElement('h1');
-  header.textContent = text;
-  return header;
+  header.style.marginBottom = '0';
+  const subhead = document.createElement('h3');
+  subhead.style.marginTop = '0';
+  if (lang === 'en') {
+    header.textContent = 'Virtual keyboard';
+    subhead.textContent = '(MacOS edition)';
+  } else if (lang === 'ru') {
+    header.textContent = 'Виртуальная клавиатура';
+    subhead.textContent = '(Создана для системы MacOS)';
+  }
+  document.body.prepend(subhead);
+  document.body.prepend(header);
 }
 
 function addTextField() {
@@ -19,22 +47,55 @@ function addTextField() {
 const keyboardSection = document.createElement('div');
 keyboardSection.classList.add('field');
 
-function createBtn(i) {
-  const button = new Btn(btnValues[i].code, btnValues[i].value.en, btnValues[i].secondValue.en);
+function createBtn(i, l) {
+  const button = new Btn(btnValues[i].code, btnValues[i].value[l], btnValues[i].secondValue[l]);
   const el = document.createElement('div');
+  const elSecondValue = document.createElement('span');
   el.classList.add('btn');
   el.textContent = button.getValue();
+  elSecondValue.textContent = button.getSecondValue();
+  elSecondValue.classList.add('btnSecondValue');
+  el.prepend(elSecondValue);
   if (el.textContent === 'Backspace') {
     el.classList.add('backspace');
+  }
+  if (el.textContent === 'Tab') {
+    el.classList.add('tab');
+  }
+  if (el.textContent === 'DEL') {
+    el.classList.add('del');
+  }
+  if (el.textContent === 'Caps Lock') {
+    el.classList.add('caps');
+  }
+  if (el.textContent === 'Shift' && i === 42) {
+    el.classList.add('shift');
+  }
+  if (el.textContent === 'ENTER') {
+    el.classList.add('enter');
+  }
+  if (el.textContent === 'Command' || i === 59 || i === 61) {
+    el.classList.add('cmnd');
+  }
+  if (el.textContent === ' ') {
+    el.classList.add('space');
   }
   return el;
 }
 
-btnValues.map((_, i) => {
-  keyboardSection.append(createBtn(i));
-  return keyboardSection;
-});
+function fillKeyboard() {
+  btnValues.map((_, i) => {
+    keyboardSection.append(createBtn(i, lang));
+    return keyboardSection;
+  });
+}
 
-document.body.prepend(createHeader('Virtual keyboard'));
+window.addEventListener('load', fillKeyboard);
+window.addEventListener('load', createHeader);
+
 document.body.append(addTextField());
 document.body.append(keyboardSection);
+
+const btns = document.querySelectorAll('.btn');
+const field = document.querySelector('textarea');
+// console.log(btns);
